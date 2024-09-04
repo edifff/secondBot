5e0ae6e1c411df8f49a0e41ea3e8a2fee794228c
edifff <mishapronin2015@gmail.com>
2024-09-04 18:56:22 +0300

    It is second step in the creating tg bot

--- node/src/main/java/bot/service/impl/MainServiceImpl.java
+++ node/src/main/java/bot/service/impl/MainServiceImpl.java
@@ -14,7 +14,6 @@ import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.User;
 
 import static bot.entity.enams.UserState.BASIC_STATE;
 import static bot.entity.enams.UserState.WAIT_FOR_EMAIL_STATE;
@@ -23,147 +22,157 @@ import static bot.service.emans.ServiceCommand.*;
 @Service
 @Log4j
 public class MainServiceImpl implements MainSevrice {
-    private final RawDataDao rawDataDao;
+    private final RawDataDao rawDataDAO;
+
     private final ProducerService producerService;
-    private final AppUserDao appUserDao;
+
+    private final AppUserDao appUserDAO;
+
     private final FileService fileService;
 
-    public MainServiceImpl(RawDataDao rawDataDao, ProducerService producerService, AppUserDao appUserDao, FileService fileService) {
-        this.rawDataDao = rawDataDao;
+    public MainServiceImpl(RawDataDao rawDataDAO, ProducerService producerService, AppUserDao appUserDAO, FileService fileService) {
+        this.rawDataDAO = rawDataDAO;
         this.producerService = producerService;
-        this.appUserDao = appUserDao;
+        this.appUserDAO = appUserDAO;
         this.fileService = fileService;
     }
 
+
     @Override
-    public void processTextMessage(Update update){
+    public void processTextMessage(Update update) {
         saveRawData(update);
-        var appUser=findOrSaveAppUser(update);
-        var userState=appUser.getState();
-        var text=update.getMessage().getText();
-        var output="";
+        var appUser = findOrSaveAppUser(update);
+        var userState = appUser.getState();
+        var text = update.getMessage().getText();
+        var output = "";
 
         var serviceCommand = ServiceCommand.fromValue(text);
-        if(CANCEL.equals(serviceCommand)){
-            output=cancelProcess(appUser);
-        } else if (BASIC_STATE.equals(serviceCommand)){
-            output=processServiceCommand(appUser, text);
-        } else if (WAIT_FOR_EMAIL_STATE.equals(serviceCommand)) {
-            //TODO after
+        if (CANCEL.equals(serviceCommand)) {
+            output = cancelProcess(appUser);
+        } else if (BASIC_STATE.equals(userState)) {
+            output = processServiceCommand(appUser, text);
+        } else if (WAIT_FOR_EMAIL_STATE.equals(userState)) {
+           ;
         } else {
-            log.error("unknown user state "+ userState);
-            output="Неизвестная ошибка! Введите /cancel и попробуйте снова!";
+            log.error("Unknown user state: " + userState);
+            output = "Неизвестная ошибка! Введите /cancel и попробуйте снова!";
         }
 
-        var chatId=update.getMessage().getChatId();
+        var chatId = update.getMessage().getChatId();
         sendAnswer(output, chatId);
-
-
-}
+    }
 
     @Override
     public void processDocMessage(Update update) {
         saveRawData(update);
-        var appUser=findOrSaveAppUser(update);
-        var chatId=update.getMessage().getChatId();
-        if (isNotAllowToSendContent(chatId, appUser)){
+        var appUser = findOrSaveAppUser(update);
+        var chatId = update.getMessage().getChatId();
+        if (isNotAllowToSendContent(chatId, appUser)) {
             return;
         }
+
         try {
-            AppDocument doc=fileService.processDoc(update.getMessage());
-            var answer= "Документ успешно загружен!"
-                    + "Download link: http://test.ru/get-doc/777";
+            AppDocument doc = fileService.processDoc(update.getMessage());
+
+            var answer = "Документ успешно загружен! "
+                    + "Ссылка для скачивания: " ;
             sendAnswer(answer, chatId);
-        }
-        //TODO app to save doc
-        catch (UploadFileException ex ){
+        } catch (UploadFileException ex) {
             log.error(ex);
-            String error="К сожалению, загрузка не удалась. Повторите попытку позже";
+            String error = "К сожалению, загрузка файла не удалась. Повторите попытку позже.";
             sendAnswer(error, chatId);
         }
+    }
 
+    @Override
+    public void processPhotoMessage(Update update) {
+        saveRawData(update);
+        var appUser = findOrSaveAppUser(update);
+        var chatId = update.getMessage().getChatId();
+        if (isNotAllowToSendContent(chatId, appUser)) {
+            return;
+        }
+
+        try {
+
+            var answer = "Фото успешно загружено! "
+                    + "Ссылка для скачивания: " ;
+            sendAnswer(answer, chatId);
+        } catch (UploadFileException ex) {
+            log.error(ex);
+            String error = "К сожалению, загрузка фото не удалась. Повторите попытку позже.";
+            sendAnswer(error, chatId);
+        }
     }
 
     private boolean isNotAllowToSendContent(Long chatId, AppUser appUser) {
-        var userState= appUser.getState();
-        if (!appUser.getIsActive()){
-            var error="арегистрируйтесь и введите свою учетную запись для загрузки контента";
+        var userState = appUser.getState();
+        if (!appUser.getIsActive()) {
+            var error = "Зарегистрируйтесь или активируйте "
+                    + "свою учетную запись для загрузки контента.";
             sendAnswer(error, chatId);
             return true;
         } else if (!BASIC_STATE.equals(userState)) {
-            var error="завершите текущую команду, чтобы продолжить";
+            var error = "Отмените текущую команду с помощью /cancel для отправки файлов.";
             sendAnswer(error, chatId);
             return true;
         }
         return false;
     }
 
-    @Override
-    public void processPhotoMessage(Update update) {
-        saveRawData(update);
-        var appUser=findOrSaveAppUser(update);
-        var chatId=update.getMessage().getChatId();
-        if (isNotAllowToSendContent(chatId, appUser)){
-            return;
-        }
-        //TODO app to save photo
-        var answer= "Photo was download. Download link: http://test.ru/get-photo/777";
-        sendAnswer(answer, chatId);
-    }
-
     private void sendAnswer(String output, Long chatId) {
-        var sendMessage= new SendMessage();
+        var sendMessage = new SendMessage();
         sendMessage.setChatId(chatId);
         sendMessage.setText(output);
         producerService.producerAnswer(sendMessage);
     }
 
-    private String processServiceCommand(AppUser appUser, String text) {
-        if(REGISTRATION.equals(text)){
-            return "Временно недоступно.";
-        } else if (HELP.equals(text)) {
+    private String processServiceCommand(AppUser appUser, String cmd) {
+        var serviceCommand = ServiceCommand.fromValue(cmd);
+        if (REGISTRATION.equals(serviceCommand)) {
+            return "Команда временно недоступна.";
+        } else if (HELP.equals(serviceCommand)) {
             return help();
-        } else if (START.equals(text)){
-            return "Приветствую! Чтобы посмотреть список доступных конанд нажмите /help";
+        } else if (START.equals(serviceCommand)) {
+            return "Приветствую! Чтобы посмотреть список доступных команд введите /help";
         } else {
-            return "неизвестная команда";
+            return "Неизвестная команда! Чтобы посмотреть список доступных команд введите /help";
         }
     }
 
     private String help() {
-        return "Список доступных команд: \n" +
-                "cancel- отмена выполнения текущей команды \n" +
-                "registration";
+        return "Список доступных команд:\n"
+                + "/cancel - отмена выполнения текущей команды;\n"
+                + "/registration - регистрация пользователя.";
     }
 
     private String cancelProcess(AppUser appUser) {
         appUser.setState(BASIC_STATE);
-        appUserDao.save(appUser);
+        appUserDAO.save(appUser);
         return "Команда отменена!";
     }
 
-    private AppUser findOrSaveAppUser(Update update){
-        User telegramUser= update.getMessage().getFrom();
-        AppUser persistentAppUser =appUserDao.findAppUserByTelegramUserId(telegramUser.getId());
-        if(persistentAppUser==null){
-            AppUser transientAppUser=AppUser.builder()
+    private AppUser findOrSaveAppUser(Update update) {
+        var telegramUser = update.getMessage().getFrom();
+        AppUser persistentAppUser = appUserDAO.findAppUserByTelegramUserId(telegramUser.getId());
+        if (persistentAppUser== null) {
+            var transientAppUser = AppUser.builder()
                     .telegramUserId(telegramUser.getId())
                     .userName(telegramUser.getUserName())
                     .firstName(telegramUser.getFirstName())
                     .lastName(telegramUser.getLastName())
-                    .isActive(true)
+                    .isActive(false)
                     .state(BASIC_STATE)
                     .build();
-        return appUserDao.save(transientAppUser);
+            return appUserDAO.save(transientAppUser);
         }
         return persistentAppUser;
     }
 
     private void saveRawData(Update update) {
-        RawData rawData =RawData.builder()
+        var rawData = RawData.builder()
                 .event(update)
                 .build();
-        rawDataDao.save(rawData);
-
+        rawDataDAO.save(rawData);
     }
 }
